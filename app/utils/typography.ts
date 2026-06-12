4cab9fd05b8565d8fd586d09044b01f593de208f
Sergey Anatska <krasilou.a@dsd-insurance.by>
2026-06-12 04:22:37 +0300

    keepTypography

--- app/utils/typography.ts
+++ app/utils/typography.ts
@@ -1,8 +1,11 @@
-export function keepShortWords(input?: string | null): string {
+export function keepTypography(input?: string | null): string {
   if (!input) return "";
 
-  return input.replace(
-    /(^|\s)([A-Za-zА-Яа-яЁёІіЇїЄєҐґ]{1,2})\s+/g,
-    "$1$2\u00A0",
-  );
+  return input
+    .replace(/(^|\s)([\p{L}]{1,2})\s+/gu, "$1$2\u00A0")
+    .replace(/\s+\(/g, "\u00A0(")
+    .replace(/\(\s+/g, "(\u00A0")
+    .replace(/([«„“"'])\s+/g, "$1\u00A0")
+    .replace(/([№§])\s+/g, "$1\u00A0")
+    .replace(/(^|\s)(—|–|-)\s+/g, "$1$2\u00A0");
 }
